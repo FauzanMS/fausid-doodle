eafb1d88e81518202932a6d242b0d5562e8a0756
FauzanMS <fauzan4mak@gmail.com>
2022-11-27 08:45:26 +0530

    first.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import * as React from "react";
 import './App.css';
+import { Whiteboard } from "react-whiteboard";
 
-function App() {
+const App = () => {
+  const [files, setFiles] = React.useState({});
+  const [resendFiles, setResendFiles] = React.useState(false);
+
+  const [canvasJSON, setCanvasJSON] = React.useState({});
+
+  React.useEffect(() => {
+    if (Object.values(files).length > 0) {
+      if (resendFiles) {
+
+      }
+      console.log(Object.values(files));
+      console.log(canvasJSON);
+      // for (let i = 0; i < Object.values(files).length; i++) {
+      //   saveAs(Object.values(files)[i], `page${i + 1}.png`);
+      // }
+    }
+    // eslint-disable-next-line
+  }, [files, resendFiles])
+
+  const color = [
+    {
+      title: 'red',
+      color: '#ff0019'
+    },
+    {
+      title: 'yellow',
+      color: '#ffc400'
+    },
+    {
+      title: 'black',
+      color: '#000000'
+    },
+    {
+      title: 'green',
+      color: '#59ff0d'
+    },
+    {
+      title: 'pink',
+      color: '#f700ff'
+    },
+    {
+      title: 'purple',
+      color: '#8000ff'
+    },
+    {
+      title: 'cyan',
+      color: '#00c9c3'
+    },
+    {
+      title: 'blue',
+      color: '#021ff7'
+    },
+  ]
+
+  const width = window.innerWidth > 610 ? 610 : window.innerWidth;
+  const height = window.innerHeight > 670 ? 670 : window.innerHeight;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main>
+        <Whiteboard aspectRatio={width / (height)} pdf="" setFiles={setFiles} setResendFiles={setResendFiles} color={color} setJSON={setCanvasJSON} pdfUrl="" resend={true} revision={false} buttonFlag={true} />
+      </main>
     </div>
   );
-}
+};
 
 export default App;
